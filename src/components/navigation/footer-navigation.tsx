6d34c807b7e9e06bf0d5bb0fa4b097fda1b8e532
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-03-08 16:55:37 +0100

    style: simple layout init

---
+++ src/components/navigation/footer-navigation.tsx
@@ -0,0 +1,10 @@
+export default function FooterNavigation() {
+  return (
+    <div className="z-50 py-4 mt-20">
+      <div className="flex gap-4">
+        <p>Link 1</p>
+        <p>Link 2</p>
+      </div>
+    </div>
+  );
+}
