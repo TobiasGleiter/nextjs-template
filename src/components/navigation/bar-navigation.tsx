71a148c75c74cbea5f0b50d1b9645fce93c03cd9
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-03-22 18:52:58 +0100

    style: added logo

--- src/components/navigation/bar-navigation.tsx
+++ src/components/navigation/bar-navigation.tsx
@@ -1,12 +1,16 @@
 import { CompanyName } from '@/src/config/company';
 import { cn } from '@/src/lib/utils';
 import Link from 'next/link';
+import Logo from '../image/logo/logo';
 import { buttonVariants } from '../ui/button';
 
 export default function NavigationBar() {
   return (
     <div className="flex w-full items-center justify-between py-4">
-      <span>{CompanyName}</span>
+      <div className="flex gap-2 items-center">
+        <Logo />
+        <span className="font-heading font-bold">{CompanyName}</span>
+      </div>
       <div className="flex gap-2 items-center">
         <Link href={'#'} className={cn(buttonVariants({ variant: 'ghost' }))}>
           Link
